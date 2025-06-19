a4493cda6001e53ffe4acf73f230b8b3b2f21e0b
Jean Li <jean841115@gmail.com>
2025-06-19 10:35:38 +0800

    upload command and its flags: file path & chunk size

--- cmd/upload.go
+++ cmd/upload.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,18 +9,45 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	filePathOptionName       = "file-path"
+	filePathShortOptionName  = "p"
+	chunkSizeOptionName      = "chunk-size"
+	chunkSizeShortOptionName = "c"
+)
+
 // uploadCmd represents the upload command
 var uploadCmd = &cobra.Command{
 	Use:   "upload",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Upload a file and split the file into chunks",
+	Long: `Upload command is going to get the uploaded file
+	and split the file into chunks. In recent, the chunked data
+	can only save into disk files.
+	You can use options like
+	--file-path to define specific file path.
+	Or 
+	--chunk-size to decide the size to split the file. ex. 1M, 1k, 256k,...`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("upload called")
+
+		// Get flags
+		filepath, err := cmd.Flags().GetString(filePathOptionName)
+		if err != nil {
+			fmt.Printf("Error getting %s option: %s", filePathOptionName, err.Error())
+			return
+		}
+
+		fmt.Println("filepath:", filepath)
+
+		chunkSize, err := cmd.Flags().GetString(chunkSizeOptionName)
+		if err != nil {
+			fmt.Printf("Error getting %s option: %s", chunkSizeOptionName, err.Error())
+			return
+		}
+
+		fmt.Println("chunkSize:", chunkSize)
+
 	},
 }
 
@@ -37,4 +63,7 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// uploadCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	uploadCmd.Flags().StringP(filePathOptionName, filePathShortOptionName, "", "File path to upload.")
+	uploadCmd.MarkFlagRequired(filePathOptionName)
+	uploadCmd.Flags().StringP(chunkSizeOptionName, chunkSizeShortOptionName, "256k", "Chunk size to split file. Default is 256kB.")
 }
